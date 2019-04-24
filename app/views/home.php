b3b868a81c52d9a02cf56baa84859644e4e2ca75
nirav-chavda <niravdchavda@gmail.com>
2019-04-24 19:35:09 +0530

    Illuminate FileSystem and Pagination added in composer

--- app/views/home.php
+++ app/views/home.php
@@ -2,7 +2,7 @@
     <div class="jumbotron jumbotron-flud text-center">
         <div class="container">
             <h1 class="display-3">Welcome To NinjaPHP</h1>
-            <img src="<?=public_path() . '/images/ninja.png' ?>" class="img-fluid" alt="Responsive image">
+            <img src="<?=public_path() . 'images/ninja.png' ?>" class="img-fluid" alt="Responsive image">
         </div>
     </div>
 <?php require_once 'layout/footer.php'; ?>
